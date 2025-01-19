117f0531ae669d8a5a11e07fd830aa7db82b8933
oleksandrcore <153992542+oleksandrcore@users.noreply.github.com>
2025-01-19 14:25:05 +0200

    Complete tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,45 @@
+class Key {
+    constructor(private signature: number = Math.random()) {}
+
+    public getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {}
+
+    public getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected key: Key;
+    protected tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    public comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    }
+
+    public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
